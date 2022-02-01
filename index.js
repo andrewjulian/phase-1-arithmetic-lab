984999bc74250637a90f0ad32f2057f3819a11ba
+ <+>
2022-02-01 12:57:38 -0700

    commit tree 23b911b77ee3f955f9dad5308dbb32ea2f4c4d6e

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+/* 1. Create a variable called `multiply` set to an equation that will multiply the
+   variables `num1` and `num2`; the result of the multiplication should be `62`.
+2. Create a variable called `random` that will generate a random integer greater
+   than 0.
+3. Create a variable called `mod` set to an equation that will calculate the
+   remainder of dividing variable `num3` by `num4`; the remainder should be `4`.
+4. Create a variable called `max` that finds the highest number in a set; the
+   value returned should be `20`.
+
+   */
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+
+let random = Math.ceil(Math.random() * 10);
+
+
+const num3 = 4;
+const num4 = 10;
+let mod = num3 % num4;
+
+let max = Math.max(1,2,3,4,20);
